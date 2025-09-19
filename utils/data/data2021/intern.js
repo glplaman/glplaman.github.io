088665289a5d6d6ae952900cb8cbbb714d843413
glpla <glpla@hotmail.com>
2025-09-19 10:16:52 +0800

    data dessert festival file

---
+++ utils/data/data2021/intern.js
@@ -0,0 +1,51 @@
+const intern = {
+  title: {
+    college: '桂林学院理工学院',
+    grade: '2018级',
+    major: '软件工程',
+    event: '自主实习情况'
+  },
+  admin: {
+    name: '张树彬',
+    nickName: 'cnplaman',
+    cell: '13707735481',
+    unit: 'pla54414',
+    email: '1942194789@qq.com'
+  },
+  students: [{
+    _id: 1001,
+    avatar: 'imgs/intern/y1.jpg',
+    name: '杨广昭',
+    cell: '18269269895',
+    grad: '已开始',
+    intern: [{
+      date: '2021-07-27',
+      sta: '已实习',
+      post: 'imgs/intern/y1.jpg'
+    }],
+    unit: {
+      title: '东软睿道教育信息技术有限公司',
+      location: '广西桂林七星区湖塘总部经济园',
+      contact: '李宽行',
+      cell: '18027108921'
+    }
+  }, {
+    _id: 1002,
+    avatar: 'imgs/intern/l1.jpg',
+    name: '卢俊铭',
+    cell: '15777457452',
+    grad: '已开始',
+    intern: [{
+      date: '2021-07-27',
+      sta: '已实习',
+      post: 'imgs/intern/l1.jpg'
+    }],
+    unit: {
+      title: '东软睿道教育信息技术有限公司',
+      location: '广西桂林七星区湖塘总部经济园',
+      contact: '李宽行',
+      cell: '18027108921'
+    }
+  }]
+}
+export default intern;
---
+++ utils/data/data2023/goods.js
@@ -0,0 +1,83 @@
+let goods = [
+  {
+    "id": 1001,
+    "title": "耐热无汞玻璃水杯",
+    "picUrl": "/utils/view/view0.jpg",
+    "price": 3,
+    "rate": 4,
+    "desc": "8 cups a day",
+    "sold": 1300
+  },
+  {
+    "id": 1002,
+    "title": "冲刺练习手册",
+    "picUrl": "/utils/view/view2.jpg",
+    "price": 10,
+    "rate": 5,
+    "desc": "more reed, more score",
+    "sold": 2500
+  },
+  {
+    "id": 1003,
+    "title": "多色记号笔",
+    "picUrl": "/utils/view/view3.jpg",
+    "price": 5,
+    "rate": 3,
+    "desc": "make colorful mark",
+    "sold": 1310
+  },
+  {
+    "id": 1004,
+    "title": "耐压防水多功能书包",
+    "picUrl": "/utils/view/view4.jpg",
+    "price": 20,
+    "rate": 4,
+    "desc": "little but big",
+    "sold": 1210
+  },
+  {
+    "id": 1005,
+    "title": "2B铅笔",
+    "picUrl": "/utils/view/view5.jpg",
+    "price": 3,
+    "rate": 2,
+    "desc": "good for you",
+    "sold": 1100
+  },
+  {
+    "id": 1006,
+    "title": "U盘",
+    "picUrl": "/utils/view/view6.jpg",
+    "price": 68,
+    "rate": 5,
+    "desc": "safe disk, safe data",
+    "sold": 1100
+  },
+  {
+    "id": 1007,
+    "title": "涂改液",
+    "picUrl": "/utils/view/view7.jpg",
+    "price": 6,
+    "rate": 3,
+    "desc": "all errors can be erased",
+    "sold": 1500
+  },
+  {
+    "id": 1008,
+    "title": "白桃橡皮",
+    "picUrl": "/utils/view/view8.jpg",
+    "price": 2,
+    "rate": 4,
+    "desc": "clear study, clear class",
+    "sold": 600
+  },
+  {
+    "id": 1009,
+    "title": "USB风扇",
+    "picUrl": "/utils/view/view9.jpg",
+    "price": 18,
+    "rate": 5,
+    "desc": "be a cool guy",
+    "sold": 2800
+  }
+]
