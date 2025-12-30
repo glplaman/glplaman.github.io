b684af28864b335f5b580333b6d1dd5aea758eb8
glpla <glpla@hotmail.com>
2025-12-30 20:05:59 +0800

    index web network

--- utils/custom/ham2024.js
+++ utils/custom/ham2024.js
@@ -1,20 +1,21 @@
 let ind = 0;
 let header = document.querySelector('header');
-let navItems = document.querySelectorAll('.nav-item')
+let items = document.querySelectorAll('.nav-item')
 let navConts = document.querySelectorAll('.nav-cont')
 let nav = document.querySelector('.nav');
 let ham = document.querySelector('.ham')
 
-navItems.forEach((item, ind) => {
+items.forEach((item, ind) => {
   item.addEventListener('click', (e) => {
     e.preventDefault()
     e.stopPropagation()
 
     header.classList.toggle('active')
-
+    
     navConts[ind].scrollIntoView()
 
-    navItems.forEach(item => {
+
+    items.forEach(item => {
       item.classList.remove('active')
     })
     item.classList.add('active');
---
+++ utils/custom/ham2025.js
@@ -0,0 +1,37 @@
+const body = document.body
+const nav = body.querySelector('nav')
+const items = body.querySelectorAll('.nav-item')
+const navConts = body.querySelectorAll('.nav-cont')
+const ham = body.querySelector('.ham')
+const toggleLock = () => {
+  if (body.classList.contains('lock')) {
+    body.classList.remove('lock');
+  }
+}
+items.forEach((item, ind) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    toggleLock()
+    navConts[ind].scrollIntoView()
+
+
+    items.forEach(item => {
+      item.classList.remove('active')
+    })
+    item.classList.add('active');
+  })
+})
+
+ham.addEventListener('click', (e) => {
+  body.classList.toggle('lock')
+})
+
+const back2top = document.querySelector(".back2top");
+document.addEventListener("scroll", () => {
+  back2top.classList.toggle("active", scrollY > innerHeight / 4);
+  nav.classList.toggle("active", scrollY > innerHeight / 4);
+  setTimeout(() => {
+    back2top.classList.remove("active");
+  }, 5000);
+});
